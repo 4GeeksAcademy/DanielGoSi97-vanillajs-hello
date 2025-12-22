640e7c477d86695d352b48c0ddc5193aa652b7b9
DanielGoSi97 <112876178+DanielGoSi97@users.noreply.github.com>
2025-12-22 23:28:15 +0000

    Refactoring

--- src/app.js
+++ src/app.js
@@ -5,16 +5,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
 let who = ['El dragon', 'El señor de la tienda', 'Messi', 'Mi jefe'];
 let action = ['se cayo en', 'se murio en', 'choco con', 'rompio'];
 let what = ['mi computador', 'mi almuerzo', 'el ascensor'];
 let when = ['cuando iba saliendo', 'mientras estaba corriendo', 'durante mi descanso', 'cuando veiamos el partido'];
 
 
-function NumRandom(arr) {
+function numRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-let GeneradorDeExcusas = NumRandom(who)+" "+NumRandom(what)+" "+NumRandom(action)+" "+NumRandom(when);
+let GeneradorDeExcusas = numRandom(who)+" "+numRandom(what)+" "+numRandom(action)+" "+numRandom(when);
 document.getElementById('excuse').innerHTML = GeneradorDeExcusas;
 }
